f1088aaa4356a3e341b39ee0e870ac251da67fa5
missu <missu@users.noreply.github.com>
2018-04-13 16:45:23 -0700

    adding scafolding

---
+++ color-theory.js
@@ -0,0 +1,59 @@
+Class ColorTheory {
+    constructor(){
+    }
+
+
+    // Converting
+    hexToRgb(hexColor) {
+    }
+
+    rgbtoHex(rgbColor) {
+
+    }
+
+    rgbaToHex(rgbaColor) {
+
+    }
+
+    nameToHex(name) {
+    }
+
+    nameToRgb(name){
+
+    }
+
+    // Ranges of Color
+    shades(color) {
+    }
+
+    tints(color) {
+    }
+
+    tones(color) {
+    }
+
+    lighten(color, percentage) {
+
+    }
+
+    darken(color, percentage) {
+    }
+
+    mute(color, percentage) {
+
+    }
+
+    //
+    mix(color1, color2) {
+
+    }
+
+    difference(color1, color2) {
+
+    }
+
+    compliment(color) {
+    }
+
+
+}
